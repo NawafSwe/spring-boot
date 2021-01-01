a6143a7437d5821dee84392d771e259bca8392cd
NawafSwe <nawaf.jr9@hotmail.com>
2021-01-01 20:57:03 +0300

    rebuilding project

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+// @Entity is a JPA annotation to make this object ready for storage in a JPA-based data store.
+@Entity
+public class Employee {
+    // generating id with generated value;
+    // @id is marked with more JPA annotations to indicate it’s the primary key and automatically populated by the JPA provider.
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+    String role;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Long primaryKey) {
+        this.id = primaryKey;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Employee)) {
+            return false;
+        }
+        Employee tempEmployee = (Employee) o;
+        return Objects.equals(this.id, tempEmployee.id) && Objects.equals(this.name, tempEmployee.name) && Objects.equals(this.role, tempEmployee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+    }
+}
---
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * There are multiple repository implementations besides JPA. You can use Spring Data MongoDB, Spring Data GemFire, Spring Data Cassandra, etc. For this tutorial, we’ll stick with JPA.
+ * Spring makes accessing data easy. By simply declaring the following EmployeeRepository interface we automatically will be able to
+ * <p>
+ * Create new Employees
+ * <p>
+ * Update existing ones
+ * <p>
+ * Delete Employees
+ * <p>
+ * Find Employees (one, all, or search by simple or complex properties)
+ **/
+//To get all this free functionality, all we had to do was declare an interface which extends Spring Data JPA’s JpaRepository,
+//specifying the domain type as Employee and the id type as Long.
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // saving employee
+    Employee save(Employee employee);
+
+    //find employee by id
+    Optional<Employee> findById(ID primaryKey);
+
+    // find all employees
+    List<Employee> findAll();
+
+    // count number of employee
+    long count();
+
+    // delete employee
+    void delete(Employee employee);
+
+    // checking if employee exists by id
+    boolean existsById(ID primaryKey);
+
+
+}
